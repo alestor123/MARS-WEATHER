863bd7f2f4a54ebbbdbc8e7696ab538b582e42ba
alestor123 <alestoraldous@gmail.com>
2021-01-04 14:16:31 +0530

    added bash

--- cli.js
+++ cli.js
@@ -1,1 +1,9 @@
+#!/usr/bin/env node
+'use strict';
+
 var weather = require('./App');
+(async () => {
+    var mars = await weather(process.argv[2]);
+    console.log('Now its ' + mars.Season + ' on Mars')
+})();
+    
